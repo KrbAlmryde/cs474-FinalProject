417de30787fae4097511df04cc8ebb7a1944de64
Kyle Almryde <kyle.almryde@gmail.com>
2016-12-02 17:58:34 -0600

    added the json protocol for extracting tweets, testing different strategies for streaming, filtering, and querying tweets

--- src/main/scala/com/finalproject/Main.scala
+++ src/main/scala/com/finalproject/Main.scala
@@ -8,38 +8,10 @@ import scala.collection.JavaConversions._
 object Main extends App {
 
 
-    println("Having a drink from the fire hose...")
+    val twitter = Tweeter.Tweeter
     val twitterStream = Tweeter.Stream
-    twitterStream.addListener(
-        new RawStreamListener {
-            override def onMessage(rawString: String): Unit = println(s"tweet: $rawString")
-            override def onException(ex: Exception): Unit = ex.printStackTrace()
-        }
-    )
-    twitterStream.sample()
-
 
 /*
-    twitterStream.addListener(
-        new StatusListener {
-            override def onStatus(status: Status): Unit = println(s"@${status.getUser.getScreenName}-${status.getText}")
-
-            override def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice): Unit = println(s"Got a status deletion notice id: $statusDeletionNotice")
-
-            override def onStallWarning(warning: StallWarning): Unit = println(s"Go stall warning: $warning")
-
-            override def onScrubGeo(userId: Long, upToStatusId: Long): Unit = println(s"Got scrub_geo event userID: $userId upToStatusId: $upToStatusId")
-
-            override def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit = println(s"Got track limitation notice: $numberOfLimitedStatuses")
-
-            override def onException(ex: Exception): Unit = ex.printStackTrace()
-        }
-    )
-    twitterStream.firehose(0)
-
-    ++++++++++++++++++++++++++++++
-    val twitter = Tweeter.Tweeter
-
     try {
         val locations = twitter.getAvailableTrends
         println("Showing available trends")
@@ -64,4 +36,56 @@ object Main extends App {
         println(s"Remaining: ${status.getRemaining}")
     })
 */
+
+
+    println("Having a drink from the fire hose...")
+// RawStream is Good, but we can do better I think...
+//    twitterStream.addListener(
+//        new RawStreamListener {
+//            override def onMessage(rawString: String): Unit = println(s"tweet: $rawString")
+//            override def onException(ex: Exception): Unit = ex.printStackTrace()
+//        }
+//    )
+//    twitterStream.sample()
+
+
+
+    twitterStream.addListener(
+        new StatusListener {
+            override def onStatus(status: Status): Unit = {
+                println(s"@${status.getUser.getScreenName}-${status.getText}")
+                val u = status.getUser
+               val out =  "@" + u.getScreenName + "\t" +
+                        u.getName + "\t" +
+                        u.getId + "\t" +
+                        u.getLocation + "\t" +
+                        u.getFollowersCount + "\t" +
+                        u.getUtcOffset + "\t" +
+                        u.getTimeZone + "\t" +
+                        u.getCreatedAt + "\t" +
+                        u.getLang + "\n" +
+                        status.getCreatedAt + "\t" +
+                        status.getGeoLocation + "\t" +
+                        status.getPlace + "\n" +
+                        status.getText
+
+                println(out)
+            }
+
+            override def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice): Unit = println(s"Got a status deletion notice id: $statusDeletionNotice")
+
+            override def onStallWarning(warning: StallWarning): Unit = println(s"Go stall warning: $warning")
+
+            override def onScrubGeo(userId: Long, upToStatusId: Long): Unit = println(s"Got scrub_geo event userID: $userId upToStatusId: $upToStatusId")
+
+            override def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit = println(s"Got track limitation notice: $numberOfLimitedStatuses")
+
+            override def onException(ex: Exception): Unit = ex.printStackTrace()
+        }
+    )
+
+    twitterStream.firehose(0)
+
 }
+
+
--- src/main/scala/com/finalproject/Utils.scala
+++ src/main/scala/com/finalproject/Utils.scala
@@ -2,11 +2,6 @@ package com.finalproject
 
 import java.io.File
 
-import twitter4j.conf.{Configuration, ConfigurationBuilder}
-
-import scala.sys.process._
-
-
 /**
   * Created by krbalmryde on 12/1/16.
   */
@@ -15,6 +10,24 @@ package object Utils {
     // This is the United States YAHOO! Where On Earth Id
     // Used for obtaining trending tweets or hashtags
     val USWOEID = 23424977
+
+
+    val instructions =
+        s"""
+           |${Console.YELLOW}
+           |** Its Twitter Time! **
+           |${Console.WHITE}
+           |
+           |Available commands include:
+           |${Console.GREEN}
+           |     t | trends ${Console.WHITE}=> Display the currently trending tweets RIGHT NOW${Console.GREEN}
+           |     q | query ${Console.WHITE}=> Enter a space separated list of terms to search the twitter-verse${Console.GREEN}
+           |     h | help ${Console.WHITE}=> Display this help message again${Console.GREEN}
+           |${Console.YELLOW}
+           |λ ---------------------------------------------- λ
+           |
+        """.stripMargin
+
 
     /**
       * Convenience function which gives me the current working directory
